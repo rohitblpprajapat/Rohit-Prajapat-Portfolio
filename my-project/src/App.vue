7a47ebf07690b6b28e54ee30345cc64bdb8e533f
blprohit <rohitblpprajapat@gmail.com>
2024-11-23 09:12:25 +0530

    Initialize Vue 3 project with Vite and Tailwind CSS setup

---
+++ my-project/src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div class="flex h-screen mx-80 flex-col justify-center items-center">
+      <img src="" alt="">
+      <div class="about flex flex-col justify-center items-center ">
+        <p class="text-gray text-center">
+          <span class="font-bold text-4xl">Namaste</span> 
+          I am a dedicated and passionate software engineer with a strong focus on delivering high-quality, innovative solutions. My journey in the tech industry has been filled with exciting challenges, rewarding projects, and continuous learning. Through this platform, I invite you to explore my world of coding, problem-solving, and creativity.
+        </p>
+    </div>
+
+  </div>
+</template>
+
+<style scoped>
+
+</style>
